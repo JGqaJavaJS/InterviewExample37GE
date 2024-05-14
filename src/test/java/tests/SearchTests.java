2fcd5ed0f4b896096750f955b65fc308df04ce5a
julia <juliiagordin@gmail.com>
2024-05-14 17:18:26 +0300

    properties

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -3,12 +3,13 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import testdata.TestDataSearch;
+import utils.ConfigProperties;
 
 public class SearchTests extends BaseTest {
 
     @Test
     public void searchPage() {
-        // TODO - Listener, properties, waits
+        // TODO -, waits
         homePage.clickOnElectonik();
         elektronikPage.clickSmartphones();
         handysSmartphonesPage.clickApple();
@@ -17,6 +18,20 @@ public class SearchTests extends BaseTest {
         applePage.fillSearchForm(res);
         Assert.assertTrue(searchResultPage
                 .verifyItemByIndexTitleEqualGiven(TestDataSearch.GET_ELEMENT_WITH_INDEX_IN_SEARCH_RESULT
+                        , res));
+    }
+
+    @Test
+    public void searchPageProperties() {
+        homePage.clickOnElectonik();
+        elektronikPage.clickSmartphones();
+        handysSmartphonesPage.clickApple();
+        String res = applePage
+                .getTextItemTitleInSearchResultByIndex(Integer.parseInt(ConfigProperties.getProperty("GET_ELEMENT_WITH_INDEX_IN_STORE")));
+        applePage.fillSearchForm(res);
+        System.out.println(ConfigProperties.getProperty("GET_ELEMENT_WITH_INDEX_IN_SEARCH_RESULT"));
+        Assert.assertTrue(searchResultPage
+                .verifyItemByIndexTitleEqualGiven(Integer.parseInt(ConfigProperties.getProperty("GET_ELEMENT_WITH_INDEX_IN_SEARCH_RESULT"))
                         , res));
     }
 }
